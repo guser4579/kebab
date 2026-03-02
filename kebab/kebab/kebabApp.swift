19503c76c37dfdc54e8a3e05e618a41efd919571
Sean Forquer <seanmforquer@gmail.com>
2026-03-02 17:44:34 -0500

    Initial clean project

---
+++ kebab/kebab/kebabApp.swift
@@ -0,0 +1,17 @@
+//
+//  kebabApp.swift
+//  kebab
+//
+//  Created by sean forquer on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct kebabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
