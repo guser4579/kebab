fe8e17ca1c7cf7fe9d480bef6bf69c052aa009cd
Sean Forquer <6774069+guser4579@users.noreply.github.com>
2026-03-03 03:18:34 -0500

    chore: stabilize supabase integration and confirm db connection

--- kebab/kebab/kebabApp.swift
+++ kebab/kebab/kebabApp.swift
@@ -1,14 +1,14 @@
-//
-//  kebabApp.swift
-//  kebab
-//
-//  Created by sean forquer on 3/2/26.
-//
-
 import SwiftUI
+import Supabase
 
 @main
 struct kebabApp: App {
+
+    let supabase = SupabaseClient(
+        supabaseURL: URL(string: "https://bjhyaqjimicxtgulvnrl.supabase.co")!,
+        supabaseKey: "sb_publishable_J7aGdl5L9Fi_cHzK9sfbdA_dkjtBReV"
+    )
+
     var body: some Scene {
         WindowGroup {
             ContentView()
